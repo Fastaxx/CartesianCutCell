ce6a5ec9d348b28ce30c2150164b1cf20a7f4017
Fastaxx <Fastaxx@github.com>
2024-02-20 15:57:05 +0100

    Add operators file

--- operators.jl
+++ operators.jl
@@ -36,6 +36,49 @@ function build_matrix_H(nx::Int, ny::Int, Ax, Ay, Bx, By)
     return H
 end
 
+# Function to create the matrix Ht
+function build_matrix_H_T(nx::Int, ny::Int, Ax, Ay, Bx, By)
+    # Create backward differentiation matrices
+    Dx_plus = forward_differentiation_matrix_sparse(nx)
+    Dy_plus = forward_differentiation_matrix_sparse(ny)
+    
+    # Compute matrix products
+    Ax_Dx_plus = Ax * Dx_plus
+    Ay_Dy_plus = Ay * Dy_plus
+    Dx_plus_Bx = Dx_plus * Bx
+    Dy_plus_By = Dy_plus * By
+    
+    # Compute blocks of H^T
+    block1 = Dx_plus_Bx - Ax_Dx_plus
+    block2 = Dy_plus_By - Ay_Dy_plus
+    
+    # Concatenate vertically to obtain H
+    Ht = hcat(block1, block2)
+    
+    return Ht
+end
+
+# Function to create the matrix -(G^T+H^T)
+function build_matrix_GTHT(nx::Int, ny::Int, Ax, Ay)
+    # Create backward differentiation matrices
+    Dx_plus = forward_differentiation_matrix_sparse(nx)
+    Dy_plus = forward_differentiation_matrix_sparse(ny)
+    
+    # Compute matrix products
+    Ax_Dx_plus = Ax * Dx_plus
+    Ay_Dy_plus = Ay * Dy_plus
+    
+    # Compute blocks of -(G^T+H^T)
+    block1 = Ax_Dx_plus
+    block2 = Ay_Dy_plus
+    
+    # Concatenate vertically to obtain H
+    GTHT = hcat(block1, block2)
+    
+    return GTHT
+end
+
+# Function to compute the grad operator
 function compute_grad_operator(pw, pg, Wt, G, H)
     # Compute G * pw
     G_pw = G * pw
@@ -49,12 +92,13 @@ function compute_grad_operator(pw, pg, Wt, G, H)
     return grad
 end
 
-function compute_divergence(qw, qg, G, H)
+# Function to compute the div operator
+function compute_divergence(qw, qg, GTHT, HT)
     # Compute -(G^T + H^T) * qw
-    div_qw = -(transpose(G) + transpose(H)) * qw
+    div_qw = GTHT * qw
     
     # Compute H^T * qg
-    div_qg = transpose(H) * qg
+    div_qg = HT * qg
     
     # Compute divergence
     divergence = div_qw + div_qg
