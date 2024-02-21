c239cac57ee9ff17d2aa43a8b698bb6bc14d5273
Fastaxx <Fastaxx@github.com>
2024-02-21 15:56:54 +0100

    enrich solve.jl with different CL

--- src/operators.jl
+++ src/operators.jl
@@ -13,11 +13,11 @@ end
 # Function to build the matrix Gt
 function build_matrix_G_T(nx::Int, ny::Int, Dx_plus, Dy_plus, Bx, By)    
     # Compute matrix products
-    Dx_plus_Bx = Dx_plus * Bx
-    Dy_plus_By = Dy_plus * By
+    Dx_plus_Bx = Bx * Dx_plus
+    Dy_plus_By = By * Dy_plus
     
     # Concatenate vertically to obtain G
-    G = hcat(Dx_plus_Bx, Dy_plus_By)
+    G = - hcat(Dx_plus_Bx, Dy_plus_By)
     
     return G
 end
@@ -43,14 +43,14 @@ end
 # Function to create the matrix Ht
 function build_matrix_H_T(nx::Int, ny::Int, Dx_plus, Dy_plus, Ax, Ay, Bx, By)
     # Compute matrix products
-    Ax_Dx_plus = Ax * Dx_plus
-    Ay_Dy_plus = Ay * Dy_plus
-    Dx_plus_Bx = Dx_plus * Bx
-    Dy_plus_By = Dy_plus * By
+    Dx_plus_Ax = Dx_plus * Ax
+    Dy_plus_Ay = Dy_plus * Ay 
+    Bx_Dx_plus = Bx * Dx_plus
+    By_Dy_plus = By * Dy_plus
     
     # Compute blocks of H^T
-    block1 = Dx_plus_Bx - Ax_Dx_plus
-    block2 = Dy_plus_By - Ay_Dy_plus
+    block1 = Bx_Dx_plus - Dx_plus_Ax
+    block2 = By_Dy_plus - Dy_plus_Ay
     
     # Concatenate vertically to obtain H
     Ht = hcat(block1, block2)
@@ -61,43 +61,43 @@ end
 # Function to create the matrix -(G^T+H^T)
 function build_matrix_GTHT(nx::Int, ny::Int, Dx_plus, Dy_plus, Ax, Ay)
     # Compute matrix products
-    Ax_Dx_plus = Ax * Dx_plus
-    Ay_Dy_plus = Ay * Dy_plus
+    Dx_plus_Ax = Dx_plus * Ax
+    Dy_plus_Ay = Dy_plus * Ay
     
     # Compute blocks of -(G^T+H^T)
-    block1 = Ax_Dx_plus
-    block2 = Ay_Dy_plus
+    block1 = Dx_plus_Ax
+    block2 = Dy_plus_Ay
     
     # Concatenate vertically to obtain H
-    GTHT = hcat(block1, block2)
+    minus_GTHT = hcat(block1, block2)
     
-    return GTHT
+    return minus_GTHT
 end
 
 # Function to compute the grad operator
-function compute_grad_operator(pw, pg, Wt, G, H)
+function compute_grad_operator(p_omega, p_gamma, Wdagger, G, H)
     # Compute G * pw
-    G_pw = G * pw
+    G_pw = G * p_omega
     
     # Compute H * pg
-    H_pgamma = H * pg
+    H_pgamma = H * p_gamma
     
-    # Compute grad = Wt * (G * pw + H * pgamma)
-    grad = Wt * (G_pw + H_pgamma)
+    # Compute grad = Wdagger * (G * p_omega + H * p_gamma)
+    grad = Wdagger * (G_pw + H_pgamma)
     
     return grad
 end
 
 # Function to compute the div operator
-function compute_divergence(qw, qg, GT, GTHT, HT, gradient::Bool=false)
+function compute_divergence(q_omega, q_gamma, GT, minus_GTHT, HT, gradient::Bool=false)
     if gradient
-        divergence = - GT * qw
+        divergence = - GT * q_omega
     else
-        # Compute -(G^T + H^T) * qw
-        div_qw = GTHT * qw
+        # Compute -(G^T + H^T) * q_omega
+        div_qw = minus_GTHT * q_omega
     
-        # Compute H^T * qg
-        div_qg = HT * qg
+        # Compute H^T * q_gamma
+        div_qg = HT * q_gamma
     
         # Compute divergence
         divergence = div_qw + div_qg
--- src/solve.jl
+++ src/solve.jl
@@ -1,9 +1,104 @@
+using SparseArrays
+using LinearAlgebra
 
+## Dirichlet Condition
 # Function to solve G^T W^T G pw = Vfw - G^T W^T H gg - Poisson Equation Dirichlet
-function solve_Ax_b_poisson(nx::Int, ny::Int, G, W, V, H, fw, gg)
-    A = transpose(G) * W * G  # Construct the matrix A
-    b = V * fw * - transpose(G) * W * H * gg # Construct the vector b
+function solve_Ax_b_poisson(nx::Int, ny::Int, G, GT, Wdagger, H, V, f_omega, g_gamma)
+    A = GT * Wdagger * G  # Construct the matrix A
+    b = V * f_omega * - GT * Wdagger * H * g_gamma # Construct the vector b
     x = A \ b       # Solve Ax = b
     return x
 end
+
+## Neumann Condition
+# Function to construct the block matrix Neumann
+function construct_block_matrix_neumann(G, GT, Wdagger, H, HT)
+    # Constructing the upper left block
+    upper_left = GT * Wdagger * G
+    
+    # Constructing the upper right block
+    upper_right = GT * Wdagger * H
+    
+    # Constructing the lower left block
+    lower_left = HT * Wdagger * G
+    
+    # Constructing the lower right block
+    lower_right = HT * Wdagger * H
+    
+    # Concatenating the blocks horizontally and vertically
+    upper_block = hcat(upper_left, upper_right)
+    lower_block = hcat(lower_left, lower_right)
+    A = vcat(upper_block, lower_block)
+    
+    return A
+end
+
+# Function to construct the RHS Vector Neumann
+function construct_rhs_vector_neumann(V, f_omega, IGamma, g_gamma)
+    # Concatenate the vectors vertically
+    b = vcat(V*f_omega, IGamma * g_gamma)
+    return b
+end
+
+# Function to solve the Bloc Matrix System Neumann
+function solve_Ax_b_neumann(G, GT, Wdagger, H, HT, V, f_omega, IGamma, g_gamma)
+    A = construct_block_matrix_neumann(G, GT, Wdagger, H, HT, Ib, Ia, IΓ)
+    b = construct_rhs_vector_neumann(V, f_omega, IGamma, g_gamma)
+
+    # Solving the linear system
+    x = A \ b
+
+    # Extracting p_ω and p_γ from the solution vector
+    p_ω = x[1:size(G, 2)]
+    p_γ = x[(size(G, 2) + 1):end]
+
+    return p_ω, p_γ
+end
+
+## Robin Condition
+# Function to construct the block matrix Robin
+function construct_block_matrix_robin(G, GT, Wdagger, H, HT, Ib, Ia, IGamma)
+    # Constructing the upper left block
+    upper_left = GT * Wdagger * G
+    
+    # Constructing the upper right block
+    upper_right = GT * Wdagger * H
+    
+    # Constructing the lower left block
+    lower_left = Ib * HT * Wdagger * G
+    
+    # Constructing the lower right block
+    lower_right = Ib * HT * Wdagger * H + Ia * IGamma
+    
+    # Concatenating the blocks horizontally and vertically
+    upper_block = hcat(upper_left, upper_right)
+    lower_block = hcat(lower_left, lower_right)
+    A = vcat(upper_block, lower_block)
+    
+    return A
+end
+
+# Function to construct the RHS Vector Robin
+function construct_rhs_vector_robin(V, f_omega, IGamma, g_gamma)
+    # Concatenate the vectors vertically
+    b = vcat(V*f_omega, IGamma * g_gamma)
+    return b
+end
+
+# Function to solve the Bloc Matrix System Robin
+function solve_Ax_b_robin(G, GT, Wdagger, H, HT, Ib, Ia, V, f_omega, IGamma, g_gamma)
+    A = construct_block_matrix_robin(G, GT, Wdagger, H, HT, Ib, Ia, IGamma)
+    b = construct_rhs_vector_robin(V, f_omega, IGamma, g_gamma)
+
+    # Solving the linear system
+    x = A \ b
+
+    # Extracting p_ω and p_γ from the solution vector
+    p_ω = x[1:size(G, 2)]
+    p_γ = x[(size(G, 2) + 1):end]
+
+    return p_ω, p_γ
+end
+
+
 
